b0e2fb01ce5d9cc82639a18b16cf9b9dada17cb7
Cyrille Gindreau <cyrille@visgence.com>
2016-12-07 17:34:59 -0700

    changing varibles into the smallest data structure possible

--- main.c
+++ main.c
@@ -1,18 +1,28 @@
 #include <msp430.h> 
-///#include <stdint.h>
+
 #define uint8_t unsigned char
 #define uint16_t unsigned int
-
-/*
- * main.c
- */
 
 #define LASER BIT0
 #define SSOUT P1OUT
 #define SSX BIT6
 #define SSY BIT7
+
+#define wrenchLength 36
+#define faceLength 54
+#define eyeLength 18
+
+//lower left wrench
+    uint8_t wrench[] = {66, 158, 47, 177, 37, 176, 27, 177, 16, 186, 8, 195, 8, 212, 27, 195, 47, 220, 27, 236, 40, 238, 56, 233, 66, 220, 68, 212, 68, 203, 86, 186, 75, 175, 66, 158};
+
+     //face
+     const uint8_t face[] = {80, 159, 127, 180, 175, 159, 187, 120, 175, 84, 162, 70, 155, 55, 146, 55, 140, 70, 137, 70, 131, 55, 121, 55, 117, 70, 115, 70, 108, 55, 98, 55, 93, 74, 80, 84, 67, 120, 80, 159, 127, 103, 130, 91, 133, 83, 127, 91, 121, 83, 125, 91, 127, 103};
+
+     //Left eye
+     uint8_t leftEye[] = {118, 125, 110, 136, 102, 138, 86, 120, 95, 105, 97, 108, 102, 114, 110, 118, 118, 125};
+
 void writeMCP492x(uint16_t data,uint8_t ss);
-void drawLine(uint16_t, uint16_t, uint16_t, uint16_t);
+void drawLine(uint8_t, uint8_t, uint8_t, uint8_t);
 
 void stall(uint16_t a){
 	uint16_t counter = a == 0 ? 8000 : 8000;
@@ -44,29 +54,15 @@ int main(void) {
      UCB0CTL1 &= ~UCSWRST;
 
 
-     //lower left wrench
-     uint16_t wrench[] = {66, 158, 47, 177, 37, 176, 27, 177, 16, 186, 8, 195, 8, 212, 27, 195, 47, 220, 27, 236, 40, 238, 56, 233, 66, 220, 68, 212, 68, 203, 86, 186, 75, 175, 66, 158};
-     uint16_t wrenchLength = 36;
 
-     //face
-     uint16_t face[] = {80, 159, 127, 180, 175, 159, 187, 120, 175, 84, 162, 70, 155, 55, 146, 55, 140, 70, 137, 70, 131, 55, 121, 55, 117, 70, 115, 70, 108, 55, 98, 55, 93, 74, 80, 84, 67, 120, 80, 159};
-     uint16_t faceLength = 40;
 
-     //Left eye
-     uint16_t leftEye[] = {118, 125, 110, 136, 102, 138, 86, 120, 95, 105, 97, 108, 102, 114, 110, 118, 118, 125};
-       uint16_t eyeLength = 18;
-
-     //Nose
-     uint16_t nose[] = {127, 103, 130, 91, 133, 83, 127, 91, 121, 83, 125, 91, 127, 103};
-     uint16_t noseLength = 14;
-
-     uint16_t myIndex = 0;
-     uint16_t logoPart = 0;
-     uint16_t mirrorCount = 4;
-     uint16_t direction = 0;
-     uint16_t i;
-     uint16_t oldX;
-     uint16_t oldY;
+     uint8_t myIndex = 0;
+     uint8_t logoPart = 0;
+     uint8_t mirrorCount = 4;
+     uint8_t direction = 0;
+     uint8_t i;
+     uint8_t oldX;
+     uint8_t oldY;
      while(1){
     	 	 if(logoPart == 0){
     	 	 	 //Draws the wrenches
@@ -78,7 +74,6 @@ int main(void) {
 						  if(mirrorCount > 1){
 							  oldX = wrench[myIndex];
 							  oldY = wrench[myIndex+1];
-
 							  for(i=0; i < wrenchLength; i++){
 								if(i%2 == 0){
 									if(direction == 0){
@@ -107,7 +102,7 @@ int main(void) {
     	 	 			mirrorCount = 2;
     	 	 			logoPart = 1;
     	 	 		 }
-    	 	 }
+    	 	 	 }
 			//draws the eyes
     	 	 	if(logoPart == 1){
     	 	 		 if(myIndex < eyeLength-3){
@@ -132,33 +127,26 @@ int main(void) {
     	 	 		} else {
     	 	 			stall(0);
     	 	 			P1OUT &= ~LASER;
-    	 	 			drawLine(leftEye[myIndex], leftEye[myIndex+1], nose[0], nose[1]);
-    	 	 			 stall(1);
+    	 	 			drawLine(leftEye[myIndex], leftEye[myIndex+1], face[0], face[1]);
+    	 	 			stall(1);
     	 	 			P1OUT |= LASER;
-    	 	 			 myIndex = 0;
+    	 	 			myIndex = 0;
     	 	 			mirrorCount = 4;
-    	 	 			 logoPart = 2;
+    	 	 			logoPart = 2;
     	 	 		}
     	 	 	}
-    	 	 	//draws the nose
+    	 	 	//draws the face and nose
     	 	 	 if(logoPart == 2){
-    	 	 		 if(myIndex < noseLength-3){
-    	 	 		    	 drawLine(nose[myIndex], nose[myIndex+1], nose[myIndex+2], nose[myIndex+3]);
-    	 	 		    	 myIndex = myIndex +2;
-    	 	 		 } else {
-    	 	 			stall(0);
-    	 	 			P1OUT &= ~LASER;
-    	 	 		    	drawLine(nose[myIndex], nose[myIndex+1], face[0], face[1]);
-    	 	 		    stall(1);
-    	 	 		    P1OUT |= LASER;
-    	 	 			logoPart = 3;
-    	 	 		    	myIndex = 0;
-    	 	 		 }
-    	 	 	 }
-    	 	 	//draws the face
-    	 	 	 if(logoPart == 3){
     	 	 		if(myIndex < faceLength-3){
+    	 	 			if(myIndex == 38){
+    	 	 				stall(1);
+    	 	 				P1OUT &= ~LASER;
+    	 	 			}
     	 	 		    	drawLine(face[myIndex], face[myIndex+1], face[myIndex+2], face[myIndex+3]);
+    	 	 		    	if(myIndex == 38){
+    	 	 		    	   stall(1);
+    	 	 		    	   P1OUT |= LASER;
+    	 	 		    	}
     	 	 		    	myIndex = myIndex +2;
     	 	 		} else {
     	 	 			stall(0);
@@ -172,19 +160,18 @@ int main(void) {
     	 	 		}
     	 	 	 }
     	 	 }
-
 }
 
 
 
-void drawLine(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2){
+void drawLine(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2){
 
-	uint16_t dx = x1 > x2 ? x1 - x2 : x2 - x1;
-	uint16_t dy = y1 > y2 ? y1 - y2 : y2 - y1;
+	uint8_t dx = x1 > x2 ? x1 - x2 : x2 - x1;
+	uint8_t dy = y1 > y2 ? y1 - y2 : y2 - y1;
 
-	uint16_t steps = dx > dy ? dx : dy;
+	uint8_t steps = dx > dy ? dx : dy;
 
-	uint16_t finalSteps = 1;
+	uint8_t finalSteps = 1;
 	while(finalSteps < steps){
 		finalSteps = finalSteps << 1;
 	}
@@ -201,7 +188,7 @@ void drawLine(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2){
 		x = x1 < x2 ? x+Xincrement : x - Xincrement;
 		y = y1 < y2 ? y+Yincrement : y - Yincrement;
 
-	    	writeMCP492x(x/8, SSX);//used to be: (x/128)*16
+	    	writeMCP492x(x/8, SSX);
 	    	writeMCP492x(y/8, SSY);
 	}
 }
